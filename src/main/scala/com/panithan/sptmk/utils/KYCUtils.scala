733e67ae03001a218a12e05fcef90e437b325254
nubbnueng <mr.nubb1@gmail.com>
2022-08-06 14:14:03 +0700

    impl getMarkedPersonalInformation

--- src/main/scala/com/panithan/sptmk/utils/KYCUtils.scala
+++ src/main/scala/com/panithan/sptmk/utils/KYCUtils.scala
@@ -3,6 +3,8 @@ package com.panithan.sptmk.utils
 import org.joda.time.{DateTimeConstants, Days}
 import org.joda.time.format.DateTimeFormat
 
+import scala.util.matching.Regex
+
 object KYCUtils {
   // Write a function that takes an Integer and returns it as a string with the correct ordinal indicator suffix (in English). Examples: 1 => 1st, 2 => 2nd.
   def getOrdinalNumberStr(number: Int): String = {
@@ -33,5 +35,20 @@ object KYCUtils {
   // Mask personal information: create a function that takes a String as input and returns it partly obfuscated. The function only recognizes emails and phone numbers, any other String that doesn’t match these types results in an error
   // Emails: emails need to be in a valid email format. To obfuscate it, it should be converted to lowercase and all characters in the local-part between the first and last should be replaced by 5 asterisks (*). Example: local-part@domain-name.com => l*****t@domain-name.com.
   // Phone numbers: a phone number consists of at least 9 digits (0-9) and may contain these two characters (‘ ‘, ‘+’) where ‘+’ is only accepted when is the first character. To obfuscate it, spaces (‘ ‘) are converted to dashes (‘-’), any digit is converted to an asterisk (‘*’) except for the last 4, which remain unchanged and the plus sign (‘+’) also remains unchanged (if present). Example: +44 123 456 789 => +**-***-**6-789.
-  def getMarkedPersonalInformation(personalInformation: String): String = ???
+  def getMarkedPersonalInformation(personalInformation: String): String = {
+    val emailAddressPattern: Regex = """^([a-zA-Z0-9.! #$%&'*+/=? ^_`{|}~-]+)@([a-zA-Z0-9-]+[\.[a-zA-Z]+]+)$""".r
+    val phoneNumberPattern: Regex = """^([+]*\d+|\d+)((\s\d|\d){4,})((\s\d|\d){4})$""".r
+
+    personalInformation.toLowerCase match {
+      case emailAddressPattern(localPart, domainName) =>
+        s"${localPart.head}*****${localPart.reverse.head}@$domainName"
+      case phoneNumberPattern(dialingCode, phoneNumberHead, _, lastFourDigits, _) =>
+        val markedDialingCode = dialingCode.replaceAll("\\d", "*")
+        val markedPhoneNumberHead = phoneNumberHead.replaceAll("\\d", "*").replaceAll("\\s", "-")
+        val markedLastFourDigits = lastFourDigits.replaceAll("\\s", "-")
+        s"$markedDialingCode$markedPhoneNumberHead$markedLastFourDigits"
+      case _ =>
+        throw new IllegalArgumentException("Input must be in either email or phone number format")
+    }
+  }
 }
